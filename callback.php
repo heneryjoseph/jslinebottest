621d70c7abbbd16b3255b90ce4907e4a2f298e48
hjslw <heneryjoseph@gmail.com>
2016-08-20 16:10:26 +0700

    ok

---
+++ callback.php
@@ -0,0 +1,6 @@
+<html>
+	<title>JS LINE BOT Testing</title>
+	<body>
+		<h1>This is Callback</h1>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+	<title>JS LINE BOT Testing</title>
+	<body>
+		<h1>This is LINE Bot testing</h1>
+	</body>
+</html>
