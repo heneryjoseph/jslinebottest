954a836c00ec4d173d0bb2f759c90aa9e1fc3de4
hjslw <heneryjoseph@gmail.com>
2016-08-21 09:11:15 +0700

    ok

--- public/index.php
+++ public/index.php
@@ -18,9 +18,9 @@
 //require_once __DIR__ . '/../vendor/autoload.php';
 
 $settings = require __DIR__ . '/src/settings.php';
-$app = new \Slim\App($settings);
+//$app = new \Slim\App($settings);
 
-require __DIR__ . '/src/dependencies.php';
+//require __DIR__ . '/src/dependencies.php';
 
 //require __DIR__ . '/src/routes.php';
 
