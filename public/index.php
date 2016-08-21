90f426df558fdf12be7522dbeefa2bef508db2a4
hjslw <heneryjoseph@gmail.com>
2016-08-21 10:19:06 +0700

    ok

--- public/index.php
+++ public/index.php
@@ -14,14 +14,14 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-require_once __DIR__ . "/../../vendor/autoload.php";
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$settings = require __DIR__ . '/src/settings.php';
+$settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-require __DIR__ . '/src/dependencies.php';
+require __DIR__ . '/../src/dependencies.php';
 
-require __DIR__ . '/src/routes.php';
+require __DIR__ . '/../src/routes.php';
 
 $app->run();
